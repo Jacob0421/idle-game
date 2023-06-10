76b9aa0df8f6aafe9fd476dbc3fd0b7664e43dad
Jacob Roberts <jacobo0421@gmail.com>
2023-06-10 00:23:00 -0400

    Added Ghosts chasing Pacman

--- src/Components/BackgroundAnimation/BackgroundAnimation.jsx
+++ src/Components/BackgroundAnimation/BackgroundAnimation.jsx
@@ -3,23 +3,69 @@ import "./BackgroundAnimation.css";
 
 export function BackgroundAnimation() {
 	return (
-		<div className="pacman-animation">
-			<div className="pacman">
-				<div className="eyes"></div>
+		<>
+			<div className="pacman-animation">
+				<div className="pacman">
+					<div className="pacman-eye"></div>
+				</div>
+				<div className="ghost blinky">
+					<div className="ghost-eyes">
+						<div className="left-eye">
+							<div className="left-pupil"></div>
+						</div>
+						<div className="right-eye">
+							<div className="right-pupil"></div>
+						</div>
+					</div>
+					<div className="tail"></div>
+				</div>
+				<div className="ghost clyde">
+					<div className="ghost-eyes">
+						<div className="left-eye">
+							<div className="left-pupil"></div>
+						</div>
+						<div className="right-eye">
+							<div className="right-pupil"></div>
+						</div>
+					</div>
+					<div className="tail"></div>
+				</div>
+				<div className="ghost inky">
+					<div className="ghost-eyes">
+						<div className="left-eye">
+							<div className="left-pupil"></div>
+						</div>
+						<div className="right-eye">
+							<div className="right-pupil"></div>
+						</div>
+					</div>
+					<div className="tail"></div>
+				</div>
+				<div className="ghost pinky">
+					<div className="ghost-eyes">
+						<div className="left-eye">
+							<div className="left-pupil"></div>
+						</div>
+						<div className="right-eye">
+							<div className="right-pupil"></div>
+						</div>
+					</div>
+					<div className="tail"></div>
+				</div>
+				<div className="pellet-container">
+					<div className="snack-pellet"></div>
+					<div className="snack-pellet"></div>
+					<div className="snack-pellet"></div>
+					<div className="snack-pellet"></div>
+					<div className="power-cookie"></div>
+					<div className="snack-pellet"></div>
+					<div className="snack-pellet"></div>
+					<div className="snack-pellet"></div>
+					<div className="snack-pellet"></div>
+					<div className="snack-pellet"></div>
+					<div className="snack-pellet"></div>
+				</div>
 			</div>
-			<div className="pellet-container">
-				<div className="snack-pellet"></div>
-				<div className="snack-pellet"></div>
-				<div className="snack-pellet"></div>
-				<div className="snack-pellet"></div>
-				<div className="power-cookie"></div>
-				<div className="snack-pellet"></div>
-				<div className="snack-pellet"></div>
-				<div className="snack-pellet"></div>
-				<div className="snack-pellet"></div>
-				<div className="snack-pellet"></div>
-				<div className="snack-pellet"></div>
-			</div>
-		</div>
+		</>
 	);
 }
